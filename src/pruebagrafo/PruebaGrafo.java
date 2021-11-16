dc0946432f8fe3995dab061847fb974d0a78d256
AndreaAd30 <andreaariasdiaz20@gmail.com>
2021-11-16 12:04:36 -0600

    Se agrega el mensaje en el main

--- src/pruebagrafo/PruebaGrafo.java
+++ src/pruebagrafo/PruebaGrafo.java
@@ -15,7 +15,7 @@ public class PruebaGrafo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
